ba3735c08b0dcd06969250cee751ad16073c0dc7
horaciomartinez-wma <horacio.martinez@warnermedia.com>
2022-12-07 10:35:19 -0600

    incluyendo archivo json

--- netlify/functions/hello/hello.go
+++ netlify/functions/hello/hello.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+   /*
    jsonData := []byte(`{
 		"name": "morpheus",
 		"job": "leader"
 	}`)
+  */
 
+  jsonData, err := os.ReadFile("socios.json")
+  if err != nil {log.Fatal(err)}
 
   return &events.APIGatewayProxyResponse{
     StatusCode:        200,
